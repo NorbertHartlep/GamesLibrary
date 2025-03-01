e5401836d08483750c7612a00d0ff2e303c41498
norbert <norbert.hartlep13@gmail.com>
2025-03-01 01:20:03 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Choose game by pressing button.");
+            System.out.println("Game 1: Brick Breaker");
+            System.out.println("Game 2: Snake");
+            System.out.println("Game 3: Pac-Man");
+            System.out.println("Game 4: Puzzle");
+            System.out.println("Game 5: Tetris");
+            System.out.println("Game 6: Move the Balls");
+            System.out.println("Game 7: Starship Defender");
+
+            int input = scanner.nextInt();
+
+            switch (input){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+            }
+    }
+    }
