92d19878120a3f1f5b63d8b6c91e22ec39dcbe69
Norbert <norbert.hartlep13@gmail.com>
2025-04-11 23:22:49 +0200

    4th game finished, 5th game in progress

--- Game_5/Tetris.java
+++ Game_5/Tetris.java
@@ -2,10 +2,39 @@ package Game_5;
 
 import utils.Game;
 
-public class Tetris implements Game {
+import javax.swing.*;
+import java.awt.*;
+
+public class Tetris extends JFrame implements Game  {
+    private JLabel statusbar;
+
+    public Tetris(){
+        initUI();
+    }
+
+    private void initUI(){
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        var board = new Board(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(200,400);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar(){
+        return statusbar;
+    }
 
     @Override
     public void init() {
-
+        EventQueue.invokeLater(() -> {
+            var game = new Tetris();
+            game.setVisible(true);
+        });
     }
 }
